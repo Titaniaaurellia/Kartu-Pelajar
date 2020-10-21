e435cb30619fbd028d803332b119248eba972bfa
Titaniaaurellia <titania_aurellia_29rpl@telkomschools.sch.id>
2020-10-21 13:09:51 +0700

    hore berhasil

---
+++ src/kartu/pelajar/KartuPelajar.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kartu.pelajar;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class KartuPelajar {
+
+    public static void main(String[] args) {
+        
+        String nama,ttl,agama,alamat,sekolah;
+        
+        nama = " Titania Aurellia Putri Dwiendra";
+        ttl = " Malang, 21 Maret 2005";
+        agama = " Islam";
+        alamat = " Jln. Sersan Murdoyo no.85";
+        sekolah = " Smk Telkom Malang";
+        
+        System.out.println("  -Kartu Pelajar- ");
+        System.out.println("Nama:" + nama);
+        System.out.println("Ttl:" + ttl);
+        System.out.println("Agama:"+agama);
+        System.out.println("Alamat:"+alamat);
+        System.out.println("Sekolah:"+sekolah);
+        
+    }
+    
+}
